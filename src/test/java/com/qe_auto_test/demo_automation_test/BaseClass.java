b549266995e6d858c950ec4397cd1b8a794152ab
mani0411 <manioverasu0411@gmail.com>
2023-07-02 11:05:00 +0530

    Initial Project

---
+++ src/test/java/com/qe_auto_test/demo_automation_test/BaseClass.java
@@ -0,0 +1,66 @@
+package com.qe_auto_test.demo_automation_test;
+
+import java.time.Duration;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	
+	public static WebDriver driver;
+	
+	String ActualTitle = "AutomationTalks - Learn Automation Testing";
+	
+	@BeforeMethod
+	public void setUp() {
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");	
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver(options);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		
+	}
+	
+	@Test
+	public void navigatoURL() {
+		driver.get("https://automationtalks.com/");
+		System.out.println(driver.getTitle());
+			
+	}
+	
+	@Test
+	public void Test1() {
+		
+		driver.get("https://automationtalks.com/");
+		String ExpTitle = driver.getTitle();
+		System.out.println("Test1 title is: "+driver.getTitle());
+		Assert.assertEquals(ActualTitle, ExpTitle);
+		
+	}
+	
+	@Test
+	public void Test2() {
+		
+		driver.get("https://automationtalks.com/");
+		String ExpTitle = driver.getTitle();
+		System.out.println("Test2 title is: "+driver.getTitle());
+		Assert.assertEquals(ActualTitle, ExpTitle);
+	}
+	
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
